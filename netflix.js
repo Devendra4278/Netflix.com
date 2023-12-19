78ae609b8bdc986092afce6cf820d186e94da993
Devendra4278 <111754140+Devendra4278@users.noreply.github.com>
2023-12-19 21:57:45 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,236 @@
+function jsNetflix1(){
+       
+            
+    let netflixButt1 = document.querySelector('.butt-js');
+    netflixButt1 = `<p>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, <br> documentaries and more-on thousands of internet-connected devices. <br>
+
+    You can watch as much as you want, whenever you want, without a single ad - all for one low monthly <br> price. There's always something new to discover, and new TV shows and movies are added every week!</p>`;
+      
+        document.querySelector('.js-result')
+            .innerHTML = netflixButt1;
+    
+}
+function nameJs1(){
+const change1 = document.querySelector('.js-msg');
+      
+    if(change1.innerHTML === 'What is Netflix ?'){
+        change1.innerHTML = 'What is Netflix +';
+        jsNetflix1();
+        return;
+
+    }else {
+        change1.innerHTML ='What is Netflix ?';
+        netflixJs1();
+        
+       
+    }
+}
+function netflixJs1(){
+let netflixButt1 = document.querySelector('.butt-js');
+netflixButt1 = `<span></span>`;
+document.querySelector('.js-result')
+    .innerHTML = netflixButt1;
+
+}
+function jsNetflix2(){
+       
+            
+    let netflixButt2 = document.querySelector('.sec-butt-js');
+    netflixButt2 = `<p>Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly <br> fee. Plans range from ₹149 to ₹649 a month. No extra costs, no contracts.</p>`;
+      
+        document.querySelector('.js-result2')
+            .innerHTML = netflixButt2;
+    
+}
+function nameJs2(){
+const change2 = document.querySelector('.js-msg2');
+
+    if(change2.innerHTML === 'How much does Netflix cost?'){
+        change2.innerHTML = 'How much does Netflix cost+';
+        jsNetflix2();
+
+    }else {
+        change2.innerHTML ='How much does Netflix cost?';
+        netflixJs2();
+        
+       
+    }
+}
+function netflixJs2(){
+let netflixButt2 = document.querySelector('.sec-butt-js');
+netflixButt2 = `<span></span>`;
+document.querySelector('.js-result2')
+    .innerHTML = netflixButt2;
+
+}
+
+function jsNetflix3(){
+       
+            
+    let netflixButt3 = document.querySelector('.third-butt-js');
+    netflixButt3 = `<p>Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly <br> fee. Plans range from ₹149 to ₹649 a month. No extra costs, no contracts.</p>`;
+      
+        document.querySelector('.js-result3')
+            .innerHTML = netflixButt3;
+    
+}
+
+function nameJs3(){
+    const change3 = document.querySelector('.js-msg3');
+    
+        if(change3.innerHTML === 'Where can I watch?'){
+            change3.innerHTML = 'Where can I watch+';
+            jsNetflix3();
+    
+        }else {
+            change3.innerHTML ='Where can I watch?';
+            netflixJs3();
+            
+           
+        }
+    }
+    function netflixJs3(){
+    let netflixButt3 = document.querySelector('.butt-js');
+    netflixButt3 = `<span></span>`;
+    document.querySelector('.js-result3')
+        .innerHTML = netflixButt3;
+    
+    }
+
+
+
+
+
+
+    function jsNetflix4(){
+       
+            
+        let netflixButt4 = document.querySelector('.four-butt-js');
+        netflixButt4 = `<p>Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your <br> account online in two clicks. There are no cancellation fees - start or stop your account anytime.</p>`;
+          
+            document.querySelector('.js-result4')
+                .innerHTML = netflixButt4;
+        
+    }
+    
+    function nameJs4(){
+        const change4 = document.querySelector('.js-msg4');
+        
+            if(change4.innerHTML === 'How do I cancel?'){
+                change4.innerHTML = 'How do I cancel+';
+                jsNetflix4();
+        
+            }else {
+                change4.innerHTML ='How do I cancel?';
+                netflixJs4();
+                
+               
+            }
+        }
+        function netflixJs4(){
+        let netflixButt4 = document.querySelector('.four-butt-js');
+        netflixButt4 = `<span></span>`;
+        document.querySelector('.js-result4')
+            .innerHTML = netflixButt4;
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        function jsNetflix5(){
+       
+            
+            let netflixButt5 = document.querySelector('.five-butt-js');
+            netflixButt5 = `<p>Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix <br> originals, and more. Watch as much as you want, anytime you want.</p>`;
+              
+                document.querySelector('.js-result5')
+                    .innerHTML = netflixButt5;
+            
+        }
+        
+        function nameJs5(){
+            const change5 = document.querySelector('.js-msg5');
+            
+                if(change5.innerHTML === 'What can I watch?'){
+                    change5.innerHTML = 'What can I watch+';
+                    jsNetflix5();
+            
+                }else {
+                    change5.innerHTML ='What can I watch?';
+                    netflixJs5();
+                    
+                   
+                }
+            }
+            function netflixJs5(){
+            let netflixButt5 = document.querySelector('.five-butt-js');
+            netflixButt5 = `<span></span>`;
+            document.querySelector('.js-result5')
+                .innerHTML = netflixButt5;
+            
+            }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+            function jsNetflix6(){
+       
+            
+                let netflixButt6 = document.querySelector('.six-butt-js');
+                netflixButt6 = `<p>Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix <br> originals, and more. Watch as much as you want, anytime you want.</p>`;
+                  
+                    document.querySelector('.js-result6')
+                        .innerHTML = netflixButt6;
+                
+            }
+            
+            function nameJs6(){
+                const change6 = document.querySelector('.js-msg6');
+                
+                    if(change6.innerHTML === 'Is Netflix good for kids?'){
+                        change6.innerHTML = 'Is Netflix good for kids+';
+                        jsNetflix6();
+                
+                    }else {
+                        change6.innerHTML ='Is Netflix good for kids?';
+                        netflixJs6();
+                        
+                       
+                    }
+                }
+                function netflixJs6(){
+                let netflixButt6 = document.querySelector('.five-butt-js');
+                netflixButt6 = `<span></span>`;
+                document.querySelector('.js-result6')
+                    .innerHTML = netflixButt6;
+                
+                }
+        
+
+
+    
+    
+    
